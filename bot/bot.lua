5541a5fe0253c2b26615ac350e379f82ebff064b
miladkh77 <milad.kh1377@gmail.com>
2016-10-05 01:46:42 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "weather",
       "xkcd",
       "youtube" },
-    sudo_users = {@miladkh77},
+    sudo_users = {166439749},
     disabled_channels = {}
   }
   serialize_to_file(config, './data/config.lua')
